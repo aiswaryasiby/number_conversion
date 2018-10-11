f644d74d8fbb690530d8b6d0e38f08f34427118a
aiswaryasiby <aiswaryasiby11@gmail.com>
2018-10-11 07:56:36 +0530

    commit

---
+++ src/main/java/Conversion.java
@@ -0,0 +1,72 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Conversion
+ */
+public class Conversion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	PrintWriter out;   
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Conversion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		out= response.getWriter(); 
+		String num1=(request.getParameter("no1"));
+		int n=Integer.parseInt(num1);
+		
+		System.out.print("After conversion number in words is :");
+        pw((n / 1000000000), " Hundred");
+        pw((n / 10000000) % 100, " crore");
+        pw(((n / 100000) % 100), " lakh");
+        pw(((n / 1000) % 100), " thousand");
+        pw(((n / 100) % 10), " hundred");
+        pw((n % 100), " ");
+        out.println(pw(n," "));
+	}
+	
+	protected String pw(int n,String ch)
+	{
+		String one[] = {
+
+		        " ", " one", " two", " three", " four", " five", " six", " seven",
+		                " eight", " Nine", " ten", " eleven", " twelve", " thirteen",
+		                " fourteen", "fifteen", " sixteen", " seventeen", " eighteen",
+		                " nineteen" };
+		        String ten[] = { " ", " ", " twenty", " thirty", " forty", " fifty",
+		                " sixty", "seventy", " eighty", " ninety" };
+		        if (n > 19) {
+		            return (ten[n / 10] + " " + one[n % 10]);
+		        } else {
+		            return (one[n]);
+		        }
+		       // if (n > 0)
+		       // 	return ch;  
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<body>
+<h2>NUMBER CONVERSION</h2>
+
+<form action="Conversion">
+<input type="text" name="no1">
+<input type="button" value="submit">
+</form>
+</body>
+</html>
